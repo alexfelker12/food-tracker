23bb9f6494614f0cb41ebabb8425275d773c483c
Alex Felker <alexanderfelker@gmx.net>
2025-10-27 20:33:56 +0100

    AuthProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,40 @@
 import type { Metadata } from "next";
-// import { Geist, Geist_Mono } from "next/font/google";
-import { ThemeProvider } from "@/components/providers/theme-provider";
+import { headers } from "next/headers";
 import "./globals.css";
+
 import { Toaster } from "@/components/ui/sonner";
 
+import { auth } from "@/lib/auth";
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { AuthProvider } from "@/components/providers/AuthProvider";
+
+
+// import { Geist, Geist_Mono } from "next/font/google";
 // const geistSans = Geist({
 //   variable: "--font-geist-sans",
 //   subsets: ["latin"],
 // });
-
 // const geistMono = Geist_Mono({
 //   variable: "--font-geist-mono",
 //   subsets: ["latin"],
 // });
-
-// ${geistSans.variable} ${geistMono.variable} 
+// -> pass as classNames to body: ${geistSans.variable} ${geistMono.variable} 
 
 export const metadata: Metadata = {
   title: "MFoody",
   description: "Track calories and your progress towards your fitness goals!",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  console.log("fetched session in root")
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="antialiased">
@@ -35,10 +44,12 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
-          <Toaster />
+          <AuthProvider initialSession={session}>
+            {children}
+            <Toaster />
+          </AuthProvider>
         </ThemeProvider>
       </body>
-    </html>
+    </html >
   );
 }
