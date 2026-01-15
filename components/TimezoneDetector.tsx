6423e3b8d0cc823bd7de0e7758fc3eb2791ad92e
Alex Felker <alexanderfelker@gmx.net>
2026-01-16 00:59:04 +0100

    - adjusted TimezoneDetector to work when timezone changes

--- components/TimezoneDetector.tsx
+++ components/TimezoneDetector.tsx
@@ -4,12 +4,16 @@ import { useEffect } from 'react';
 
 export function TimezoneDetector() {
   useEffect(() => {
+    //* get user timezone
+    const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
     const currentCookie = document.cookie
       .split('; ')
       .find(row => row.startsWith('user-timezone='))
+      ?.split('=')[1]
 
-    if (!currentCookie) {
-      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone
+    //* if timezone changed, set new timezone
+    // -> makes the app reactive to change of timezone. Should correctly handle the "today" redirect because the app gets launched at a different URL
+    if (currentCookie !== timezone) {
       document.cookie = `user-timezone=${timezone}; path=/; max-age=31536000; SameSite=Lax`
     }
   }, [])
